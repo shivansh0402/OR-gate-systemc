754c73b9841a95405cf5daf6e9d874cd22deed35
shivansh0402 <105986175+shivansh0402@users.noreply.github.com>
2023-07-22 11:18:54 +0530

    design.cpp

--- design.cpp
+++ design.cpp
@@ -7,11 +7,12 @@ SC_MODULE(example)
 	sc_in<bool>clk;
 	sc_in<bool>a;
 	sc_in<bool>b;
+	sc_in<bool>c;
 	sc_out<bool>output;
 
 	void result()
 	{
-		output.write(a.read() | b.read());
+		output.write(a.read() | b.read() |c.read());
 	}
 	SC_CTOR(example)
 	{
